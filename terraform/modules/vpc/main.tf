2fc6cb157a153cb48702d356eb990129d92dfed4
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:32:50 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,11 +1,13 @@
-resource "google_compute_network" "custom_vpc" {
+resource "google_compute_network" "vpc_network" {
   name                    = var.network_name
   auto_create_subnetworks = false
+  project                 = var.project_id
 }
 
-resource "google_compute_subnetwork" "custom_subnet" {
+resource "google_compute_subnetwork" "subnetwork" {
   name          = "${var.network_name}-subnet"
   ip_cidr_range = "10.10.0.0/16"
   region        = var.region
-  network       = google_compute_network.custom_vpc.self_link
+  network       = google_compute_network.vpc_network.id
+  project       = var.project_id
 }
