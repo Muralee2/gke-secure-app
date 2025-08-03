b149a5775a40a61c6d1d38aa4e84ca63ad77ef5c
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:08:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "your-tf-state-bucket"  # Replace with your actual bucket name
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
-terraform {
-  required_providers {
-    google     = { source = "hashicorp/google" }
-    cloudflare = { source = "cloudflare/cloudflare" }
-  }
 }
 
 module "vpc" {
@@ -29,38 +32,9 @@ module "firewall" {
 }
 
 module "gke" {
-  source      = "./modules/gke"
-  project_id  = var.project_id
-  region      = var.region
-  network     = module.vpc.network_name
-  subnetwork  = module.vpc.subnetwork_name
-}
-
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-resource "helm_release" "whereami" {
-  name       = "whereami"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
-  namespace  = "default"
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
-
-  set {
-    name  = "ingress.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "ingress.hostname"
-    value = var.domain_name
-  }
+  source     = "./modules/gke"
+  project_id = var.project_id
+  region     = var.region
+  network    = module.vpc.network_name
+  subnetwork = module.vpc.subnetwork_name
 }
