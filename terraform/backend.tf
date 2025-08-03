32ea53ee75ddd5b30da3a7908cbf7a6e6418c401
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 21:44:28 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "my-project-og"  # Replace with your bucket name
+    bucket = "project-og"  # Replace with your bucket name
     prefix = "gke-secure-app"
   }
 }
