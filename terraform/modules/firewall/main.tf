c74ad0a9b8b244c03f957a35bda56289094ce01b
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:38:22 +0530

    Update main.tf

--- terraform/modules/firewall/main.tf
+++ terraform/modules/firewall/main.tf
@@ -1,26 +1,30 @@
-data "external" "firewall_rules" {
+data "external" "rule_names" {
   program = ["python3", "${path.module}/generate_firewall_rules.py"]
 }
 
-resource "google_compute_firewall" "allow_egress_to_master" {
-  name    = data.external.firewall_rules.result["egress_rule_name"]
+resource "google_compute_firewall" "internal" {
+  name    = data.external.rule_names.result["internal_rule_name"]
+  project = var.project_id
   network = var.network
 
-  direction = "EGRESS"
-  destination_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
+  allows {
+    protocol = "all"
   }
-}
-
-resource "google_compute_firewall" "allow_internal" {
-  name    = data.external.firewall_rules.result["internal_rule_name"]
-  network = var.network
 
   direction = "INGRESS"
   source_ranges = ["10.10.0.0/16"]
-  allow {
-    protocol = "all"
+}
+
+resource "google_compute_firewall" "egress" {
+  name    = data.external.rule_names.result["egress_rule_name"]
+  project = var.project_id
+  network = var.network
+
+  allows {
+    protocol = "tcp"
+    ports    = ["443"]
   }
+
+  direction          = "EGRESS"
+  destination_ranges = ["172.16.0.0/28"] # typical GKE master CIDR, customize as needed
 }
