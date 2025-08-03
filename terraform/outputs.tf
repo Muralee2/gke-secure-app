07f72c2e61b9c408ba28ddc0e08f5cc1d6e59f83
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:09:47 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,24 @@
-output "gke_endpoint" {
-  value = module.gke.endpoint
+output "vpc_network_name" {
+  description = "The name of the VPC network"
+  value       = module.vpc.network_name
 }
 
+output "vpc_subnetwork_name" {
+  description = "The name of the subnetwork"
+  value       = module.vpc.subnetwork_name
+}
+
+output "gke_cluster_name" {
+  description = "Name of the GKE cluster"
+  value       = module.gke.cluster_name
+}
+
+output "gke_cluster_endpoint" {
+  description = "GKE cluster endpoint"
+  value       = module.gke.endpoint
+}
+
+output "gke_cluster_ca_certificate" {
+  description = "GKE cluster CA certificate"
+  value       = module.gke.ca_certificate
+}
