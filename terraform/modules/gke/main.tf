b2a5f1896e9563db4ae25605a3472263a616a32f
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:30:06 +0530

    Update main.tf

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -1,46 +1,36 @@
-resource "google_container_cluster" "gke" {
-  name     = "secure-gke"
+resource "google_container_cluster" "gke_cluster" {
+  name     = var.cluster_name
   location = var.region
-  project  = var.project_id
-
-  network    = var.network
-  subnetwork = var.subnetwork
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
-  }
-
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "0.0.0.0/0"
-      display_name = "open"
-    }
-  }
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  logging_service    = "logging.googleapis.com/kubernetes"
-  monitoring_service = "monitoring.googleapis.com/kubernetes"
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "gke-node-pool"
-  cluster    = google_container_cluster.gke.name
-  location   = var.region
-  node_count = 2
-
   node_config {
-    machine_type = "e2-medium"
+    service_account = var.gke_service_account
+    disk_type       = "pd-standard"
+    disk_size_gb    = 50
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
+
+resource "google_container_node_pool" "primary_nodes" {
+  name     = "primary-node-pool"
+  cluster  = google_container_cluster.gke_cluster.name
+  location = var.region
+
+  node_count = 1
+
+  node_config {
+    preemptible     = false
+    machine_type    = "e2-medium"
+    disk_type       = "pd-standard"
+    disk_size_gb    = 50
+    service_account = var.gke_service_account
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
