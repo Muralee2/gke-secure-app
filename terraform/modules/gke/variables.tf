2feb68377ce285d6e1b37f18f7d8f0fa473e1e87
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:30:45 +0530

    Update variables.tf

--- terraform/modules/gke/variables.tf
+++ terraform/modules/gke/variables.tf
@@ -1,4 +1,14 @@
-variable "project_id" {}
-variable "region" {}
-variable "network" {}
-variable "subnetwork" {}
+variable "cluster_name" {
+  type        = string
+  description = "Name of the GKE cluster"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to deploy GKE cluster"
+}
+
+variable "gke_service_account" {
+  type        = string
+  description = "Service account email to use for node pool"
+}
