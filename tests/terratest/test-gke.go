a268ca81a0d5af20cc0ae66609e586b31b7bcfe9
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:11:35 +0530

    Update test-gke.go

--- tests/terratest/test-gke.go
+++ tests/terratest/test-gke.go
@@ -3,6 +3,7 @@ package test
 import (
   "testing"
   "strings"
+
   "github.com/gruntwork-io/terratest/modules/terraform"
   "github.com/stretchr/testify/assert"
 )
@@ -11,29 +12,26 @@ func TestGKECluster(t *testing.T) {
   t.Parallel()
 
   terraformOptions := &terraform.Options{
-    // Path to where your Terraform code is located
+    // Adjust to your Terraform code directory
     TerraformDir: "../../terraform",
 
-    // Variables to pass to Terraform
     Vars: map[string]interface{}{
-      "project_id":            "your-gcp-project-id",
-      "region":                "asia-south1",
-      "zone":                  "asia-south1-a",
-      "network_name":          "custom-vpc",
-      "domain_name":           "your.domain.com",
-      "cloudflare_zone_id":    "your-zone-id",
-      "cloudflare_api_token":  "your-token",
+      "project_id":    "your-gcp-project-id",
+      "region":        "asia-south1",
+      "zone":          "asia-south1-a",
+      "network_name":  "custom-vpc",
     },
   }
 
-  // Defer destroy
+  // Clean up resources at the end
   defer terraform.Destroy(t, terraformOptions)
 
-  // Init and apply
+  // Init and apply the Terraform code
   terraform.InitAndApply(t, terraformOptions)
 
-  // Get output
+  // Test output
   gkeEndpoint := terraform.Output(t, terraformOptions, "gke_endpoint")
 
-  assert.True(t, strings.Contains(gkeEndpoint, "."), "GKE endpoint should contain a dot")
+  // Basic validation
+  assert.Contains(t, gkeEndpoint, ".", "GKE endpoint should look like a valid URL")
 }
