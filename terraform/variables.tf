963ee5df93ffd7fea431b5a4b77b1781918eccb7
Muralee2 <muralimvkaws1@gmail.com>
2025-08-03 22:09:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,22 @@
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
-variable "network_name" {}
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+  default     = "gke-vpc-network"
+}
