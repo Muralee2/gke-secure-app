18a9444b9d68ef54a6615791ba41b9655dfb8606
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 01:08:50 +0530

    Update outputs.tf

--- terraform/modules/firewall/outputs.tf
+++ terraform/modules/firewall/outputs.tf
@@ -1,12 +1,3 @@
-output "status" {
-  value       = "Firewall rules applied"
-  description = "Confirmation output"
-}
-
-output "internal_rule_name" {
-  value = google_compute_firewall.internal.name
-}
-
-output "egress_rule_name" {
-  value = google_compute_firewall.egress.name
+output "firewall_name" {
+  value = google_compute_firewall.allow_ssh_http.name
 }
