eb42591aef9ea631965b447133dc6bcd23423cbe
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 01:07:35 +0530

    Update main.tf

--- terraform/modules/firewall/main.tf
+++ terraform/modules/firewall/main.tf
@@ -1,30 +1,12 @@
-data "external" "rule_names" {
-  program = ["python3", "${path.module}/generate_firewall_rules.py"]
-}
-
-resource "google_compute_firewall" "internal" {
-  name    = data.external.rule_names.result["internal_rule_name"]
-  project = var.project_id
+resource "google_compute_firewall" "allow_ssh_http" {
+  name    = "allow-ssh-http"
   network = var.network
 
-  allows {
-    protocol = "all"
-  }
-
-  direction = "INGRESS"
-  source_ranges = ["10.10.0.0/16"]
-}
-
-resource "google_compute_firewall" "egress" {
-  name    = data.external.rule_names.result["egress_rule_name"]
-  project = var.project_id
-  network = var.network
-
-  allows {
+  allow {
     protocol = "tcp"
-    ports    = ["443"]
+    ports    = ["22", "80", "443"]
   }
 
-  direction          = "EGRESS"
-  destination_ranges = ["172.16.0.0/28"] # typical GKE master CIDR, customize as needed
+  source_ranges = ["0.0.0.0/0"]
 }
+
