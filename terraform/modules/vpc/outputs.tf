4e03bf580d9f920fc57e0df29bdf01f745003f1d
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 01:14:24 +0530

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,14 +1,7 @@
 output "network_name" {
-  value       = google_compute_network.vpc_network.name
-  description = "The name of the created VPC network"
+  value = google_compute_network.vpc_network.name
 }
 
 output "subnetwork_name" {
-  value       = google_compute_subnetwork.subnetwork.name
-  description = "The name of the created subnet"
-}
-
-output "subnetwork_self_link" {
-  value       = google_compute_subnetwork.subnetwork.self_link
-  description = "The self-link of the subnet"
+  value = google_compute_subnetwork.subnetwork.name
 }
