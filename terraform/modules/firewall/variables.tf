9a35b29a928611a7c05162496343c9a38cd4b3b0
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 01:09:11 +0530

    Update variables.tf

--- terraform/modules/firewall/variables.tf
+++ terraform/modules/firewall/variables.tf
@@ -1,9 +1,2 @@
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "network" {
-  description = "VPC network name"
-  type        = string
-}
+variable "project_id" {}
+variable "network" {}
