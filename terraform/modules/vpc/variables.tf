82572cef039c6651cafe87f8081d9143885ec163
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 01:14:58 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,14 +1,3 @@
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "Region where resources will be created"
-  type        = string
-}
-
-variable "network_name" {
-  description = "Name of the VPC network"
-  type        = string
-}
+variable "project_id" {}
+variable "region" {}
+variable "network_name" {}
